c694bba1ae42acc7c1392187a95f960ea1cfa99a
Dylan Farrell <dylanfarrell@Dylans-MacBook-Pro-4.local>
2017-09-07 12:02:07 -0400

    new submission

--- HW0/util.py
+++ HW0/util.py
@@ -1,7 +1,7 @@
 ##### Filename: util.py
-##### Author: {your name}
-##### Date: {current date}
-##### Email: {your email}
+##### Author: {Dylan Farrell}
+##### Date: {9/7/17}
+##### Email: {dylanfarrell@college.harvard.edu}
 
 import copy
 from collections import deque
@@ -9,56 +9,83 @@ from collections import deque
 ## Problem 1
 
 def matrix_multiply(x, y):
-    pass
+    def dot(x, y):
+        n = sum([a*b for a, b in zip(x, y)])
+        return n
+    lsts = []
+    for i in x:
+        lst = []
+        for j in range(0, len(y[0])):
+            lst.append(dot(i, [row[j] for row in y]))
+        lsts.append(lst)
+    return lsts
+
 
 ## Problem 2, 3
 
 class MyQueue:
     def __init__(self):
-        pass
+        self.data = deque([])
     def push(self, val):
-        pass
+        self.data.append(val)
     def pop(self):
-        pass
+        if self.data:
+            copy = self.data[0]
+            del self.data[0]
+            return copy
+        else:
+            return None
     def __eq__(self, other):
-        pass
+        return self.data == other.data
     def __ne__(self, other):
-        pass
+        return not(self.data == other.data)
     def __str__(self):
-        pass
+        return str(self.data)
 
 class MyStack:
     def __init__(self):
-        pass
+        self.data = deque([])
     def push(self, val):
-        pass
+        self.data.append(val)
     def pop(self):
-        pass
+        if self.data == []:
+            return None
+        else:
+            copy = self.data[-1]
+            del self.data[-1]
+            return copy
     def __eq__(self, other):
-        pass
+        return self.data == other.data
     def __ne__(self, other):
-        pass
+        return not(self.data == other.data)
     def __str__(self):
-        pass
+        return str(self.data)
 
 ## Problem 4
 
 def add_position_iter(lst, number_from=0):
-    pass
+    return [a+number_from+i for i, a in enumerate(lst)]
 
 def add_position_recur(lst, number_from=0):
-    pass
+    if lst:
+        frst = [lst[0]+number_from]
+        a = number_from+1
+        return frst+(add_position_recur(lst[1:len(lst)], number_from=a))
+    else:
+        return lst
 
 def add_position_map(lst, number_from=0):
-    pass
+    return list(map(lambda (x): x[0]+x[1]+number_from, enumerate(lst)))
 
 ## Problem 5
 
 def remove_course(roster, student, course):
-    pass
+    roster[student].remove(course)
+    return roster
 
 ## Problem 6
 
 def copy_remove_course(roster, student, course):
-    pass
-
+    c = copy.deepcopy(roster)
+    remove_course(c, student, course)
+    return c
